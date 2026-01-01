ed8611417ed86038ea6251a112b6338b5d3d66c2
Emmanuel Bourret <emmanuel.bourret@umontreal.ca>
2026-01-01 10:27:50 -0500

    delete renamed directory

--- legacy_script/very_old/Misc/test.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-filename="${1##*/}"
-filepref="${filename%%.*}"
-
-sbatch --job-name=$filepref --export=All,FILE=$1 run_orca.sbatch
--- legacy_script/very_old/orca_UV-vis.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-FILE=$1
-
-module load StdEnv/2020 gcc/10.3.0 openmpi/4.1.1
-module load orca/5.0.2
-
-$EBROOTORCA/orca_mapspc $FILE Abs -x07500 -x134000 -w500 -n5000
-#$EBROOTORCA/orca_mapspc $FILE Abs -eV -x00 -x110 -w0.01
